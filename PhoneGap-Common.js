79c1f39fcb8022037558396bea97c306ca1d1099
paulodiff <paulo.difficiliora@gmail.com>
2013-08-08 12:15:34 +0200

    Started Push Notification

--- PhoneGap-Common.js
+++ PhoneGap-Common.js
@@ -71,6 +71,10 @@ function onDeviceReady() {
     
     // execute a callback function
     //executeCallback();
+	
+	// register push notifications...
+	RegisterPushNotification();
+	
 }
 
 function executeEvents() {
@@ -290,7 +294,85 @@ function onResume() {
                 'message: ' + error.message + '\n');
     }
 	
+// Push notifications ---------------------------------------------------------------------
 
+
+
+	function PushNotificationSuccessHandler(result){
+		alert('Callback Success! Result = '+result)
+	}
+	
+	function PushNotificationErrorHandler(error){
+		alert(error);
+	}
+	
+	function PushNotificationTokenHandler(msg){
+		console.log("Token Handler " + msg);
+	}
+
+
+	function RegisterPushNotification(){
+	
+		console.log('RegisterPushNotification ... ');
+	
+		var pushNotification = window.plugins.pushNotification;
+        // TODO: Enter your own GCM Sender ID in the register call for Android
+        if (device.platform == 'android' || device.platform == 'Android') {
+            pushNotification.register(PushNotificationSuccessHandler, PushNotificationErrorHandler,{"senderID":"1045204524713","ecb":"PushNotificationAndroid"});
+        }
+        else {
+            pushNotification.register(PushNotificationTokenHandler,PushNotificationErrorHandler,{"badge":"true","sound":"true","alert":"true","ecb":"PushNotificationIOS"});
+        }
+        console.log('RegisterPushNotification: ' + id);
+    }
+	
+	
+	function PushNotificationAndroid(event) {
+		switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    // Your GCM push server needs to know the regID before it can push to this device
+                    // here is where you might want to send it the regID for later use.
+                    alert('registration id = '+e.regid);
+                }
+            break;
+
+            case 'message':
+              // this is the actual push notification. its format depends on the data model
+              // of the intermediary push server which must also be reflected in GCMIntentService.java
+              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+            break;
+
+            case 'error':
+              alert('GCM error = '+e.msg);
+            break;
+
+            default:
+              alert('An unknown GCM event has occurred');
+              break;
+        }
+	}
+	
+	function PushNotificationIOS(event){
+		var pushNotification = window.plugins.pushNotification;
+        console.log("Received a notification! " + event.alert);
+        console.log("event sound " + event.sound);
+        console.log("event badge " + event.badge);
+        console.log("event " + event);
+        if (event.alert) {
+            navigator.notification.alert(event.alert);
+        }
+        if (event.badge) {
+            console.log("Set badge on  " + pushNotification);
+            pushNotification.setApplicationIconBadgeNumber(this.successHandler, event.badge);
+        }
+        if (event.sound) {
+            var snd = new Media(event.sound);
+            snd.play();
+        }
+	}
 
 /*
 // This gets called by jQuery mobile when the page has loaded
